6968747e4ec936b57e91643d0f1aec6986e88429
bengi2nginjen <eusebia.fernanda@ti.ukdw.ac.id>
2020-06-09 10:51:10 +0700

    First chapter part 2

---
+++ web/db.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "dbtugas5.cenzylrufou4.us-east-1.rds.amazonaws.com";
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$database = getnev('MYSQL_DATABASE');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database) or die("Can't connect to database");
+
+// Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
+?>
+<?php
+//session_start();
+
+//$conn = mysqli_connect(
+//  'localhost',
+//  'root',
+//  'password123',
+//  'php_mysql_crud'
+//) or die(mysqli_erro($mysqli));
+
+?>
